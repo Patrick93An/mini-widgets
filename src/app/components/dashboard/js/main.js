f221253692610f5d1d094b0234434ea08c183216
Patrick Low <lowyuenan@gmail.com>
2018-09-20 03:57:07 +0800

    minor fix

--- src/app/components/dashboard/js/main.js
+++ src/app/components/dashboard/js/main.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Provider } from "react-redux";
 import { store } from "./store"
-import Dashboard from "./Dashboard"
+import Dashboard from "./dashboard"
 
 
 export default class DashboardMain extends React.Component {
